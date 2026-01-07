a3e8ef932cce7c91babc7b5caa1a34bff3b22292
Congjie He <congjiehe95@gmail.com>
2026-01-07 20:04:54 +0000

    Update .simd_64=true to .simd_max=true

--- Decode/WSE-3/benchmark.sh
+++ Decode/WSE-3/benchmark.sh
@@ -10,10 +10,16 @@
 ./run_wse3.sh model_config/llama8B_4k_1_660.json false | tee log/decode_wse3_llama8B_4k_1_660.log
 ./run_wse3.sh model_config/llama8B_4k_1_720.json false | tee log/decode_wse3_llama8B_4k_1_720.log
 
-./run_wse3.sh model_config/qwen72B_4k_1_360.json false | tee log/decode_wse3_qwen72B_4k_1_360.log
-./run_wse3.sh model_config/qwen72B_4k_1_420.json false | tee log/decode_wse3_qwen72B_4k_1_420.log
-./run_wse3.sh model_config/qwen72B_4k_1_480.json false | tee log/decode_wse3_qwen72B_4k_1_480.log
-./run_wse3.sh model_config/qwen72B_4k_1_540.json false | tee log/decode_wse3_qwen72B_4k_1_540.log
-./run_wse3.sh model_config/qwen72B_4k_1_600.json false | tee log/decode_wse3_qwen72B_4k_1_600.log
-./run_wse3.sh model_config/qwen72B_4k_1_660.json false | tee log/decode_wse3_qwen72B_4k_1_660.log
-./run_wse3.sh model_config/qwen72B_4k_1_720.json false | tee log/decode_wse3_qwen72B_4k_1_720.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_360.json false | tee log/decode_wse3_qwen72B_4k_1_360.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_420.json false | tee log/decode_wse3_qwen72B_4k_1_420.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_480.json false | tee log/decode_wse3_qwen72B_4k_1_480.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_540.json false | tee log/decode_wse3_qwen72B_4k_1_540.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_600.json false | tee log/decode_wse3_qwen72B_4k_1_600.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_660.json false | tee log/decode_wse3_qwen72B_4k_1_660.log
+# ./run_wse3.sh model_config/qwen72B_4k_1_720.json false | tee log/decode_wse3_qwen72B_4k_1_720.log
+
+# ./run_wse3.sh model_config/llama8B_4k_1_360.json false | tee log/decode_wse3_llama8B_4k_1_360.log
+# ./run_wse3.sh model_config/llama8B_4k_4_360.json false | tee log/decode_wse3_llama8B_4k_4_360.log
+# ./run_wse3.sh model_config/llama8B_4k_16_360.json false | tee log/decode_wse3_llama8B_4k_16_360.log
+# ./run_wse3.sh model_config/llama8B_4k_64_360.json false | tee log/decode_wse3_llama8B_4k_64_360.log
+# ./run_wse3.sh model_config/llama8B_4k_128_360.json false | tee log/decode_wse3_llama8B_4k_128_360.log
